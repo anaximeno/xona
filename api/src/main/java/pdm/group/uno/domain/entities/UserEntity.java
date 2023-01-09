e94867acdb7b983259e052b338f3da7c6b111f68
anaximeno <anaximenobrito@gmail.com>
2023-01-09 00:49:02 -0100

    refactor: remove unused imports

--- api/src/main/java/pdm/group/uno/domain/entities/UserEntity.java
+++ api/src/main/java/pdm/group/uno/domain/entities/UserEntity.java
@@ -3,9 +3,6 @@ package pdm.group.uno.domain.entities;
 import java.time.LocalDate;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Column;
 
