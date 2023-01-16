8116d6b61284b6d35e18f931dd997e377572760d
Aniel Freire <anielfreire@gmail.com>
2023-01-16 18:23:10 -0100

    fix bug

--- android/app/src/main/java/pdm/group/uno/xona/InitialActivity.java
+++ android/app/src/main/java/pdm/group/uno/xona/InitialActivity.java
@@ -3,12 +3,16 @@ package pdm.group.uno.xona;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class InitialActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(getWindow().FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_initial);
     }
 }
