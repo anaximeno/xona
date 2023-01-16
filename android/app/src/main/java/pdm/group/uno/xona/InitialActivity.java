cb50784d1ee81d4be8d1044278c6ebca0ddfe793
Aniel Freire <anielfreire@gmail.com>
2023-01-16 14:49:33 -0100

    add initial activity

---
+++ android/app/src/main/java/pdm/group/uno/xona/InitialActivity.java
@@ -0,0 +1,14 @@
+package pdm.group.uno.xona;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InitialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initial);
+    }
+}
